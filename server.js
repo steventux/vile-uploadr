ef434ce4cb6342464d03ce0a565a5222a60edfe8
Steve <steve.laing@gmail.com>
2012-03-26 17:56:43 +0100

    Port number fix

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 // Use express at the static file server.
-var app = express.createServer();
+var app = express.createServer(),
+    port = process.env.PORT || 8080;
 
 app.get('/', function(req, res){
     res.send('Hello World');
 });
     
 // Start the appserver.     
-app.listen(80);
+app.listen(port);
