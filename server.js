c8dce9360aa9975096954856ccfd9563aa414065
Steve <steve.laing@gmail.com>
2012-03-26 17:53:06 +0100

    Wildcarding...

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 // Use express at the static file server.
 var app = express.createServer();
 
-app.get('/', function(req, res){
+app.get('/*', function(req, res){
     res.send('Hello World');
 });
     
